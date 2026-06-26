0b789c499d3627f0e04b5049ff3e8b21f497c6c5
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-06-27 00:56:22 +0600

    fact(server): integrate database initialization with app startup in main function

---
+++ src/db/index.ts
@@ -0,0 +1,48 @@
+import { Pool } from "pg";
+import config from "../config";
+
+// PostgreSQL connection pool using Neon cloud database
+export const pool = new Pool({
+    connectionString: config.connection_string,
+});
+
+// Setup PostgreSQL schema and create table if it doesn't exist
+export const initDB = async () => {
+    try {
+
+        // create 'users' table
+        // await pool.query(`
+        // CREATE TABLE IF NOT EXISTS users(
+        // id SERIAL PRIMARY KEY,
+        // name VARCHAR(20),
+        // email VARCHAR(20) UNIQUE NOT NULL,
+        // password TEXT NOT NULL,
+        // is_active BOOLEAN DEFAULT true,
+        // age INT,
+        // role VARCHAR(10) DEFAULT 'user',
+
+        // created_at TIMESTAMP DEFAULT NOW(),
+        // updated_at TIMESTAMP DEFAULT NOW()
+        // )
+        // `)
+        console.log("Database connected successfully!");
+
+        // create 'profile' table
+        // await pool.query(`
+        // CREATE TABLE IF NOT EXISTS profiles(
+        // id SERIAL PRIMARY KEY,
+        // user_id INT UNIQUE REFERENCES users(id) ON DELETE CASCADE,
+
+        // bio TEXT,
+        // address TEXT,
+        // phone VARCHAR(15),
+        // gender VARCHAR(10),
+
+        // created_at TIMESTAMP DEFAULT NOW(),
+        // updated_at TIMESTAMP DEFAULT NOW()
+        // )
+        // `)
+    } catch (error) {
+        console.log(error);
+    }
+}
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
-// import app from "./app";
-// import config from "./config";
-// import { initDB } from "./db";
+import app from "./app";
+import config from "./config";
+import { initDB } from "./db";
 
-// const main = () => {
-//     initDB();
-//     app.listen(config.port, () => {
-//         console.log(`Server is running at http://localhost:${config.port}`);
-//     });
-// };
+const main = () => {
+    initDB();
+    app.listen(config.port, () => {
+        console.log(`Server is running at http://localhost:${config.port}`);
+    });
+};
 
-// main();
+main();
