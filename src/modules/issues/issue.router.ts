df3796c53cc31e209f2038c84101f6c92ba05d7e
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-06-28 23:59:47 +0600

    feat(issue.router.ts): add routes for creating and retrieving issues with issueController integration

--- src/modules/issues/issue.router.ts
+++ src/modules/issues/issue.router.ts
@@ -4,5 +4,7 @@ import { issueController } from "./issue.controller";
 const router = Router();
 
 router.post('/', issueController.createIssue);
+router.get('/', issueController.getAllIssues);
+
 
 export const issueRouter = router;
